7ff2a8b4145875949fc28852552517b61eb3fa73
Jordan Coque <jordancoque1211@gmail.com>
2024-05-31 22:38:40 -0400

    Initial Commit

---
+++ Contact_Information.js
@@ -0,0 +1,8 @@
+// Student's Name: Jordan Coque
+// Student ID: 301273107
+// Date: 2024-05-31
+document.getElementById('contact_Form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your message!');
+    window.location.href = 'index.html';
+});
---
+++ Services.js
@@ -0,0 +1,11 @@
+// Student's Name: Jordan Coque
+// Student ID: 301273107
+// Date: 2024-05-31
+function toggleList(listId) {
+    var list = document.getElementById(listId);
+    if (list.style.display === 'none') {
+        list.style.display = 'block';
+    } else {
+        list.style.display = 'none';
+    }
+}
